f324e6c09791efd625d13abb18be10b6ff395534
james.schuepbach@cubetech.ch <james.schuepbach@cubetech.ch>
2014-06-04 07:44:43 +0200

    added action

--- cubetech-icon-slider.php
+++ cubetech-icon-slider.php
@@ -48,8 +48,9 @@ if(!function_exists('enqueue_css'))
 		wp_register_style('custom_jquery-ui-dialog', plugins_url('assets/css/jquery-ui-dialog.min.css', __FILE__) );
 		wp_enqueue_style('custom_jquery-ui-dialog');
 	}
+add_action( 'admin_enqueue_scripts', 'enqueue_css' ); 
 } 
- 
+
 function register_cubetech_icon_slider_button($buttons) {
    array_push($buttons, "|", "cubetech_icon_slider_button");
    return $buttons;
